bc3f6ee6d6163d58505a079882c507130ff46d70
Luo Tianj <lt@basecity.com>
2020-06-02 03:03:58 +0800

    add some unit tests

--- test/robertluo/remote_pull/format_test.clj
+++ test/robertluo/remote_pull/format_test.clj
@@ -3,6 +3,37 @@
    [robertluo.remote-pull.format :as sut]
    [clojure.test :refer [deftest is testing]]))
 
+(deftest decode
+  (testing "delegate to format"
+    (is (= :a (sut/decode (sut/->EdnFormatter) ":a"))))
+  (testing "throws exception if malfored"
+    (is (thrown? clojure.lang.ExceptionInfo (sut/decode (sut/->EdnFormatter) "]")))))
+
+(deftest with-format
+  (let [handler (sut/with-format identity)]
+    (is (= [{:foo 3} "{:foo 3}"]
+           (->> (handler {:headers {"content-type" "application/edn"} :body "{:foo 3}"})
+                ((juxt :body-params #(sut/-decode (sut/->EdnFormatter) (:body %)))))))))
+
+(deftest with-pull
+  (let [handler (sut/with-pull (constantly {:foo "bar"}) :body-params)]
+    (testing "pull with pattern returns result"
+      (is (= {:foo "bar"}
+             (handler {:body-params :foo}))))
+    (testing "if no pattern, throw exception"
+      (is (thrown? clojure.lang.ExceptionInfo (handler {}))))))
+
+(deftest with-exception
+  (testing "for any ExceptionInfo throws, returns a 400 error response"
+    (is (= {:status 400 :body "{:message \"\"}"}
+           ((sut/with-exception (fn [_] (throw (ex-info "" {})))) {}))))
+  (testing "for other exception, returns a 500 error"
+    (is (= {:status 500 :body "java.lang.Exception: "}
+           ((sut/with-exception (fn [_] (throw (Exception. "")))) {}))))
+  (testing "default just call handler"
+    (is (= {}
+           ((sut/with-exception identity) {})))))
+
 (deftest round-trip
   (let [edn          (sut/->EdnFormatter)
         transit-json (sut/->TransitFormatter :msgpack)
